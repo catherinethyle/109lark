6ab5770d52c2b9eb2063babc6d197004b403bd26
catherine thy le <72633780+catherinethyle@users.noreply.github.com>
2024-03-12 00:57:09 -0700

    net art project

---
+++ net-art/js/main.js
@@ -0,0 +1,117 @@
+annotationOne = document.querySelector("#annotation1");
+imgAnnotationOne = document.querySelector("#img-annotation-1");
+annotationTwo = document.querySelector("#annotation2");
+imgStanleyAlbum = document.querySelector("#img-annotation-2");
+annotationThree = document.querySelector("#annotation3");
+imgAnnotationThree = document.querySelector("#img-annotation-3");
+annotationFour = document.querySelector("#annotation4");
+imgAnnotationFour = document.querySelector("#img-annotation-4");
+annotationFive = document.querySelector("#annotation5");
+imgAnnotationFive = document.querySelector("#img-annotation-5");
+annotationSix = document.querySelector("#annotation6");
+imgAnnotationSix = document.querySelector("#img-annotation-6");
+annotationSeven = document.querySelector("#annotation7");
+imgAnnotationSeven = document.querySelector("#img-annotation-7");
+annotationEight = document.querySelector("#annotation8");
+imgAnnotationEight = document.querySelector("#img-annotation-8");
+annotationNine = document.querySelector("#annotation9");
+imgAnnotationNine = document.querySelector("#img-annotation-9");
+imgAnnotationNineHalf = document.querySelector("#img-annotation-9half");
+annotationTen = document.querySelector("#annotation10");
+imgAnnotationTen = document.querySelector("#img-annotation-10");
+
+
+annotationOne.addEventListener("click", function (){
+    imgAnnotationOne.style.visibility = "visible";
+    annotationOne.style.color = "white";
+})
+
+imgAnnotationOne.addEventListener("click", function (){
+    imgAnnotationOne.style.visibility = "hidden";
+})
+
+annotationTwo.addEventListener("click", function (){
+    imgStanleyAlbum.style.visibility = "visible";
+    annotationTwo.style.color = "white";
+})
+
+imgStanleyAlbum.addEventListener("click", function (){
+    imgStanleyAlbum.style.visibility = "hidden";
+})
+
+annotationThree.addEventListener("click", function() {
+    imgAnnotationThree.style.visibility = "visible";
+    annotationThree.style.color = "white";
+})
+
+imgAnnotationThree.addEventListener("click", function() {
+    imgAnnotationThree.style.visibility = "hidden";
+})
+
+annotationFour.addEventListener("click", function() {
+    imgAnnotationFour.style.visibility = "visible";
+    annotationFour.style.color = "white";
+})
+
+imgAnnotationFour.addEventListener("click", function() {
+    imgAnnotationFour.style.visibility = "hidden";
+})
+
+annotationFive.addEventListener("click", function() {
+    imgAnnotationFive.style.visibility = "visible";
+    annotationFive.style.color = "white";
+})
+
+imgAnnotationFive.addEventListener("click", function() {
+    imgAnnotationFive.style.visibility = "hidden";
+})
+
+annotationSix.addEventListener("click", function() {
+    imgAnnotationSix.style.visibility = "visible";
+    annotationSix.style.color = "white";
+})
+
+imgAnnotationSix.addEventListener("click", function() {
+    imgAnnotationSix.style.visibility = "hidden";
+})
+
+annotationSeven.addEventListener("click", function() {
+    imgAnnotationSeven.style.visibility = "visible";
+    annotationSeven.style.color = "white";
+})
+
+imgAnnotationSeven.addEventListener("click", function() {
+    imgAnnotationSeven.style.visibility = "hidden";
+})
+
+annotationEight.addEventListener("click", function() {
+    imgAnnotationEight.style.visibility = "visible";
+    annotationEight.style.color = "white";
+})
+
+imgAnnotationEight.addEventListener("click", function() {
+    imgAnnotationEight.style.visibility = "hidden";
+})
+
+annotationNine.addEventListener("click", function() {
+    imgAnnotationNine.style.visibility = "visible";
+    imgAnnotationNineHalf.style.visibility = "visible";
+    annotationNine.style.color = "white";
+})
+
+imgAnnotationNine.addEventListener("click", function() {
+    imgAnnotationNine.style.visibility = "hidden";
+})
+
+imgAnnotationNineHalf.addEventListener("click", function() {
+    imgAnnotationNineHalf.style.visibility = "hidden";
+})
+
+annotationTen.addEventListener("click", function() {
+    imgAnnotationTen.style.visibility = "visible";
+    annotationTen.style.color = "white";
+})
+
+imgAnnotationTen.addEventListener("click", function() {
+    imgAnnotationTen.style.visibility = "hidden";
+})
