e367f8f95e9f175fd1d3d2413f0af7b2e7177421
catherine thy le <72633780+catherinethyle@users.noreply.github.com>
2024-04-30 10:16:18 -0600

    immersive web update

--- immersive-web/js/main.js
+++ immersive-web/js/main.js
@@ -3,14 +3,14 @@ import * as THREE from 'three';
 
 import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
 
-let scene, camera, renderer, bedTexture, shelfTexture, titleTexture, titleMesh, doorTexture, floorTexture, closetTexture;
+let scene, camera, renderer, titleTexture, titleMesh, bedWallTexture, family1Texture, family2Texture, family3Texture, family4Texture, family5Texture, family6Texture, family7Texture, family8Texture, pcWallTexture, doorWallTexture, floorTexture, ceilingTexture, closetWallTexture;
 let sceneContainer = document.querySelector("#scene-container");
 
-function init(){
+function init() {
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x79C5F3);
 
-    const lightRight = new THREE.DirectionalLight(0xFDEDDB, 3); 
+    const lightRight = new THREE.DirectionalLight(0xFDEDDB, 3);
     lightRight.position.set(4, 2, 5);
     scene.add(lightRight);
 
@@ -25,95 +25,124 @@ function init(){
         2000 //set camera for far plane
     );
 
+    //adding title geometry
     const titleTextureLoader = new THREE.TextureLoader();
     titleTexture = titleTextureLoader.load('./assets/looking-@t-home.png');
-    const titleMaterial = new THREE.MeshBasicMaterial({ map: titleTexture, transparent: true});
+    const titleMaterial = new THREE.MeshBasicMaterial({ map: titleTexture, transparent: true });
     const titleGeometry = new THREE.BoxGeometry(0, 1, 1);
     titleMesh = new THREE.Mesh(titleGeometry, titleMaterial);
-    titleMesh.position.set(3, 0, 0);
+    titleMesh.position.set(2, 0, 0);
     scene.add(titleMesh);
 
-    const bedTextureLoader = new THREE.TextureLoader();
-    bedTexture = bedTextureLoader.load('./assets/bed-wall.png');
-    const bedMaterial = new THREE.MeshBasicMaterial({ map: bedTexture, transparent: true});
-    const bedGeometry = new THREE.BoxGeometry(11.62, 7, 0);
-    const bedMesh = new THREE.Mesh(bedGeometry, bedMaterial);
-    bedMesh.position.set(1.75, 0, -3.3);
-    scene.add(bedMesh);
+    //adding bed wall
+    const bedWallTextureLoader = new THREE.TextureLoader();
+    bedWallTexture = bedWallTextureLoader.load('./assets/bed-wall.png');
+    const bedWallMaterial = new THREE.MeshBasicMaterial({ map: bedWallTexture, transparent: true });
+    const bedWallGeometry = new THREE.BoxGeometry(11.62, 7, 0);
+    const bedWallMesh = new THREE.Mesh(bedWallGeometry, bedWallMaterial);
+    bedWallMesh.position.set(1.75, 0, -3.3);
+    scene.add(bedWallMesh);
 
-    // const drawerTextureLoader = new THREE.TextureLoader();
-    // drawerTexture = drawerTextureLoader.load('./assets/drawer.PNG');
-    // const drawerMaterial = new THREE.MeshBasicMaterial({ map: drawerTexture, transparent: true});
-    // const drawerGeometry = new THREE.BoxGeometry(7, 7, 0);
-    // const drawerMesh = new THREE.Mesh(drawerGeometry, drawerMaterial);
-    // drawerMesh.position.set(6.5, 0, -4);
-    // scene.add(drawerMesh);
+    //adding family pictures behind window
+    const familyTextureLoader = new THREE.TextureLoader();
+    family1Texture = familyTextureLoader.load('./assets/family-1.png');
+    family2Texture = familyTextureLoader.load('./assets/family-2.png');
+    family3Texture = familyTextureLoader.load('./assets/family-3.png');
+    family4Texture = familyTextureLoader.load('./assets/family-4.png');
+    family5Texture = familyTextureLoader.load('./assets/family-5.png');
+    family6Texture = familyTextureLoader.load('./assets/family-6.png');
+    family7Texture = familyTextureLoader.load('./assets/family-7.png');
+    family8Texture = familyTextureLoader.load('./assets/family-8.png');
+    const familyMaterial = new THREE.MeshBasicMaterial({ map: family1Texture });
+    const familyGeometry = new THREE.BoxGeometry(4.54, 2, 0);
+    const familyMesh = new THREE.Mesh(familyGeometry, familyMaterial);
+    familyMesh.position.set(0.35, 0.93, -3.4);
+    scene.add(familyMesh);
 
-    const shelfTextureLoader = new THREE.TextureLoader();
-    shelfTexture = shelfTextureLoader.load('./assets/pc-wall.png');
-    const shelfMaterial = new THREE.MeshBasicMaterial({ map: shelfTexture, transparent: true});
-    const shelfGeometry = new THREE.BoxGeometry(11.62, 7, 0); //width, heiight, depth
-    const shelfMesh = new THREE.Mesh(shelfGeometry, shelfMaterial);
-    shelfMesh.position.set(1.75, 0, 3.3);
-    scene.add(shelfMesh);
+    // Array of textures
+    const textures = [
+        family1Texture,
+        family2Texture,
+        family3Texture,
+        family4Texture,
+        family5Texture,
+        family6Texture,
+        family7Texture,
+        family8Texture,
+    ];
 
-    const doorTextureLoader = new THREE.TextureLoader();
-    doorTexture = doorTextureLoader.load('./assets/door-wall.png');
-    const doorMaterial = new THREE.MeshBasicMaterial({ map: doorTexture, transparent: true});
-    const doorGeometry = new THREE.BoxGeometry(0, 7, 7);
-    const doorMesh = new THREE.Mesh(doorGeometry, doorMaterial);
-    doorMesh.position.set(-4, 0, 0);
-    scene.add(doorMesh);
+    let currentTextureIndex = 0;
 
-    const closetTextureLoader = new THREE.TextureLoader();
-    closetTexture = closetTextureLoader.load('./assets/closet-wall.png');
-    const closetMaterial = new THREE.MeshBasicMaterial({ map: closetTexture, transparent: true});
-    const closetGeometry = new THREE.BoxGeometry(0, 7, 7);
-    const closetMesh = new THREE.Mesh(closetGeometry, closetMaterial);
-    closetMesh.position.set(7.5, 0, 0);
-    scene.add(closetMesh);
+    // Function to change texture after 2 seconds
+    function changeTexture() {
+        currentTextureIndex = (currentTextureIndex + 1) % textures.length;
+        familyMesh.material.map = textures[currentTextureIndex];
+    }
 
-    // const rugTextureLoader = new THREE.TextureLoader();
-    // rugTexture = rugTextureLoader.load('./assets/rug.png');
-    // const rugMaterial = new THREE.MeshBasicMaterial({ map: rugTexture, transparent: true});
-    // const rugGeometry = new THREE.BoxGeometry(7, 0, 7);
-    // const rugMesh = new THREE.Mesh(rugGeometry, rugMaterial);
-    // rugMesh.position.set(0, -3.5, 0); // x, y, z
-    // scene.add(rugMesh);
+    // Change texture after 2 seconds and repeat every 2 seconds
+    setInterval(changeTexture, 2000);
 
+    //adding pc wall
+    const pcWallTextureLoader = new THREE.TextureLoader();
+    pcWallTexture = pcWallTextureLoader.load('./assets/pc-wall.png');
+    const pcWallMaterial = new THREE.MeshBasicMaterial({ map: pcWallTexture, transparent: true });
+    const pcWallGeometry = new THREE.BoxGeometry(11.62, 7, 0); //width, heiight, depth
+    const pcWallMesh = new THREE.Mesh(pcWallGeometry, pcWallMaterial);
+    pcWallMesh.position.set(1.75, 0, 3.3);
+    scene.add(pcWallMesh);
+
+    //adding door wall
+    const doorWallTextureLoader = new THREE.TextureLoader();
+    doorWallTexture = doorWallTextureLoader.load('./assets/door-wall.png');
+    const doorWallMaterial = new THREE.MeshBasicMaterial({ map: doorWallTexture, transparent: true });
+    const doorWallGeometry = new THREE.BoxGeometry(0, 7, 7);
+    const doorWallMesh = new THREE.Mesh(doorWallGeometry, doorWallMaterial);
+    doorWallMesh.position.set(-4, 0, 0);
+    scene.add(doorWallMesh);
+
+    //adding closet wall
+    const closetWallTextureLoader = new THREE.TextureLoader();
+    closetWallTexture = closetWallTextureLoader.load('./assets/closet-wall.png');
+    const closetWallMaterial = new THREE.MeshBasicMaterial({ map: closetWallTexture, transparent: true });
+    const closetWallGeometry = new THREE.BoxGeometry(0, 7, 7);
+    const closetWallMesh = new THREE.Mesh(closetWallGeometry, closetWallMaterial);
+    closetWallMesh.position.set(7.5, 0, 0);
+    scene.add(closetWallMesh);
+
+    //adding floor 
     const floorTextureLoader = new THREE.TextureLoader();
     floorTexture = floorTextureLoader.load('./assets/floor-rug.png');
-    const floorMaterial = new THREE.MeshBasicMaterial({ map: floorTexture, transparent: true});
+    const floorMaterial = new THREE.MeshBasicMaterial({ map: floorTexture, transparent: true });
     const floorGeometry = new THREE.BoxGeometry(14.2, 0, 7.2);
     const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
     floorMesh.position.set(2.2, -3.6, 0); // x, y, z
     scene.add(floorMesh);
 
-    renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+    //adding ceiling
+    const ceilingTextureLoader = new THREE.TextureLoader();
+    ceilingTexture = ceilingTextureLoader.load('./assets/ceiling.png');
+    const ceilingMaterial = new THREE.MeshBasicMaterial({ map: ceilingTexture, transparent: true });
+    const ceilingGeometry = new THREE.BoxGeometry(14.2, 0, 7.2);
+    const ceilingMesh = new THREE.Mesh(ceilingGeometry, ceilingMaterial);
+    ceilingMesh.position.set(2.2, 3.4, 0); // x, y, z
+    scene.add(ceilingMesh);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
     renderer.setSize(sceneContainer.clientWidth, sceneContainer.clientHeight);
     sceneContainer.appendChild(renderer.domElement);
 
     const controls = new OrbitControls(camera, renderer.domElement);
-
-    // const controls = new THREE.FirstPersonControls(camera, renderer.domElement);
-
-    // // set the initial position and direction on the controls
-    // controls.lookSpeed = 0.1;
-    // controls.movementSpeed = 10;
-    // controls.noFly = true;
-    // controls.lookVertical = true;
 
     camera.position.z = 2;
     camera.position.x = 2;
 
 }
 
-function animate(){
-    requestAnimationFrame(animate);
-    // controls.update();
-    renderer.render(scene, camera);
-
+function animate() {
     titleMesh.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 }
 
 function onWindowResize() {
--- immersive-web/js/sound.js
+++ immersive-web/js/sound.js
@@ -1,14 +1,9 @@
 let voicemail = document.querySelector("#voicemail");
 let playButton = document.querySelector("#play-button");
-let pauseButton = document.querySelector("#mute-button");
 
 playButton.addEventListener("click", function() {
     voicemail.play();
     voicemail.volume = 0.65;
-})
-
-pauseButton.addEventListener("click", function() {
-    voicemail.pause();
 })
 
 voicemail.onloadeddata = function() {
