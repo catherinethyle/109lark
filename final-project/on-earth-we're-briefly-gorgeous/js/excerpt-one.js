88bac09aeae8e763c9421287f92244289b3b47d2
catherine thy le <72633780+catherinethyle@users.noreply.github.com>
2024-05-09 20:28:57 -0500

    asset upload

---
+++ final-project/on-earth-we're-briefly-gorgeous/js/excerpt-one.js
@@ -0,0 +1,17 @@
+let canvas;
+
+function setup(){
+    canvas = createCanvas(, );
+    canvas.parent("excerpt-one-p5");
+}
+
+function preload(){
+    backgroundImg = loadImage('./img/grid-paper.png');
+}
+
+function draw(){
+    background(backgroundImg);
+}   
+
+function mouseMoved(){
+}
